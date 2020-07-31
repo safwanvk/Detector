1c7f2ea3406b6af8c8b8a651655a4f9c0a10ca3d
safwanvk <safwanvalakundil@gmail.com>
2020-07-31 18:02:11 +0530

    :white_check_mark: Set smile-detection

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ while True:
     face=face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
     for x,y,w,h in face:
         img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
-       
+        smile=smile_cascade.detectMultiScale(gray,scaleFactor=1.8,minNeighbors=20)
+        for x,y,w,h in smile:
+            img=cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
 
-    cv2.imshow('Zab-Face-Detec',frame)
+    cv2.imshow('Zab-Face-Detect',frame)
     key=cv2.waitKey(1)
 
     if key==ord('q'):
